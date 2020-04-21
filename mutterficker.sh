348af7c5a89d0f67c35f55687e5286ff4986a707
GottModusTermux <40678640+GottModusTermux@users.noreply.github.com>
2020-04-21 15:19:33 +0200

    Create mutterficker.sh
    bash this script

---
+++ mutterficker.sh
@@ -0,0 +1,57 @@
+#!/data/data/com.termux/files/usr/bin/bash -e
+
+# colors
+
+red='\033[1;31m'
+reset='\033[0m'
+
+printf "$red"
+echo "MutterFick prozess gestartet.."
+
+sleep 2
+echo
+echo
+echo
+echo "start"
+echo
+sleep 3
+echo "1%"
+sleep 2
+echo "3%"
+sleep 3
+echo "7%"
+sleep 1
+echo "12%"
+sleep 1
+echo "24%"
+sleep 4
+echo "64%"
+sleep 2
+echo "81%"
+sleep 1
+echo "92%"
+sleep 1
+echo "100%"
+sleep 2
+echo
+echo "Fertig !!"
+printf "$reset"
+
+sleep 3
+
+clear
+
+printf "$red"
+echo "Gratulation.."
+sleep 2
+echo
+echo
+echo "Mütter wurden erfolgreich gefickt"
+sleep 3
+echo "MutterFick prozess erfolgreich durchgefürht !!"
+printf "$reset"
+
+clear
+ls
+
+exit
